06b305403dba4bb76ef4554106cb9e9000f3a785
Nicolas Lebon <Nicolas.Lebon@plumettaz.com>
2025-04-27 17:20:00 +0200

    Fix task status format

--- backend/populate.sql
+++ backend/populate.sql
@@ -21,24 +21,24 @@ INSERT INTO user_projects (user_id, project_id) VALUES (5, 2); -- Luana dans mp1
 
 -- Ajouter des tâches pour le projet "optijet"
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (1, 'Task 1 for Optijet', 'Description for Task 1', 'To Do', 1, 1, '2023-04-01T10:00:00Z', '2023-04-01T10:00:00Z');
+VALUES (1, 'Task 1 for Optijet', 'Description for Task 1', 'todo', 1, 1, '2023-04-01T10:00:00Z', '2023-04-01T10:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (1, 'Task 2 for Optijet', 'Description for Task 2', 'In Progress', 2, 1, '2023-04-02T11:00:00Z', '2023-04-02T11:00:00Z');
+VALUES (1, 'Task 2 for Optijet', 'Description for Task 2', 'inProgress', 2, 1, '2023-04-02T11:00:00Z', '2023-04-02T11:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (1, 'Task 3 for Optijet', 'Description for Task 3', 'Done', 3, 1, '2023-04-03T12:00:00Z', '2023-04-03T12:00:00Z');
+VALUES (1, 'Task 3 for Optijet', 'Description for Task 3', 'done', 3, 1, '2023-04-03T12:00:00Z', '2023-04-03T12:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (1, 'Task 4 for Optijet', 'Description for Task 4', 'To Do', 2, 1, '2023-04-04T13:00:00Z', '2023-04-04T13:00:00Z');
+VALUES (1, 'Task 4 for Optijet', 'Description for Task 4', 'todo', 2, 1, '2023-04-04T13:00:00Z', '2023-04-04T13:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (1, 'Task 5 for Optijet', 'Description for Task 5', 'In Progress', 3, 1, '2023-04-05T14:00:00Z', '2023-04-05T14:00:00Z');
+VALUES (1, 'Task 5 for Optijet', 'Description for Task 5', 'inProgress', 3, 1, '2023-04-05T14:00:00Z', '2023-04-05T14:00:00Z');
 
 -- Ajouter des tâches pour le projet "mp100"
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (2, 'Task 1 for MP100', 'Description for Task 1', 'To Do', 4, 4, '2023-04-01T10:00:00Z', '2023-04-01T10:00:00Z');
+VALUES (2, 'Task 1 for MP100', 'Description for Task 1', 'todo', 4, 4, '2023-04-01T10:00:00Z', '2023-04-01T10:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (2, 'Task 2 for MP100', 'Description for Task 2', 'In Progress', 5, 4, '2023-04-02T11:00:00Z', '2023-04-02T11:00:00Z');
+VALUES (2, 'Task 2 for MP100', 'Description for Task 2', 'inProgress', 5, 4, '2023-04-02T11:00:00Z', '2023-04-02T11:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (2, 'Task 3 for MP100', 'Description for Task 3', 'Done', 4, 4, '2023-04-03T12:00:00Z', '2023-04-03T12:00:00Z');
+VALUES (2, 'Task 3 for MP100', 'Description for Task 3', 'done', 4, 4, '2023-04-03T12:00:00Z', '2023-04-03T12:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (2, 'Task 4 for MP100', 'Description for Task 4', 'To Do', 5, 4, '2023-04-04T13:00:00Z', '2023-04-04T13:00:00Z');
+VALUES (2, 'Task 4 for MP100', 'Description for Task 4', 'todo', 5, 4, '2023-04-04T13:00:00Z', '2023-04-04T13:00:00Z');
 INSERT INTO tasks (project_id, title, description, status, assigned_user_id, created_by, created_date, modified_date)
-VALUES (2, 'Task 5 for MP100', 'Description for Task 5', 'In Progress', 4, 4, '2023-04-05T14:00:00Z', '2023-04-05T14:00:00Z');
+VALUES (2, 'Task 5 for MP100', 'Description for Task 5', 'inProgress', 4, 4, '2023-04-05T14:00:00Z', '2023-04-05T14:00:00Z');
